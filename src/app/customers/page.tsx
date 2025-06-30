29215162daf0655deac40897303e0062e78d706c
Hùng Nguyễn <duyhung1632002@gmail.com>
2025-06-30 11:40:05 +0700

    add navbar sidebar

---
+++ src/app/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomersPage() {
+    return (
+        <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <h1>Customers Management Page</h1>
+        </div>
+    );
+}
---
+++ src/app/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function OrdersPage() {
+    return (
+        <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <h1>Orders Management Page</h1>
+        </div>
+    );
+}
