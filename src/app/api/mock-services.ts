d9748ca815de5b815c003bcf2f59336bbef37926
Hùng Nguyễn <duyhung1632002@gmail.com>
2025-07-02 16:40:43 +0700

    :zap: update UserOrder filter

---
+++ src/app/api/mock-services.ts
@@ -0,0 +1,7 @@
+import { Service } from "../type/services";
+
+export const mockServices: Service[] = [
+    { id: '1', name: 'Cleaning Service' },
+    { id: '2', name: 'Plumbing Service' },
+    { id: '3', name: 'Electrical Service' },
+];
